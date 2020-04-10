4e7b8754a3dad3816ea0e5a383d23f183c623c38
Kamil Kalota <kamilkalota@gmail.com>
2020-04-10 19:28:21 +0200

    Updates

---
+++ precache-manifest.4656ec560ad69f62e71cec69f7528b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5112a914b4f5f3eecdaca2b253a94bdb",
+    "url": "/moja-kolejna-proba-gh-pages/index.html"
+  },
+  {
+    "revision": "9bf783f70b0e0b905e27",
+    "url": "/moja-kolejna-proba-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05a327eb951e9c1f0c2b",
+    "url": "/moja-kolejna-proba-gh-pages/static/js/2.233d56bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moja-kolejna-proba-gh-pages/static/js/2.233d56bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf783f70b0e0b905e27",
+    "url": "/moja-kolejna-proba-gh-pages/static/js/main.d915060e.chunk.js"
+  },
+  {
+    "revision": "a14a5a6be23235b413f9",
+    "url": "/moja-kolejna-proba-gh-pages/static/js/runtime-main.7e1793d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moja-kolejna-proba-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
